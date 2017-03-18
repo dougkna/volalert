e92a2947d1873eac462b8949b05f23888b5fd7c4
Doug K Na <dougkna@gmail.com>
2017-03-18 12:28:24 -0700

    finall

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var request = require('request')
 var Ticker = require('../models/ticker');
+var findOrCreate = require('mongoose-findorcreate')
 Ticker.plugin(findOrCreate);
 var Subs = require('../models/subscription');
 var Price = require('../models/price');
